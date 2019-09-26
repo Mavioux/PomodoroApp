9f86593d25d3e1be0ea8e0e78fe6908a9a6b1b78
mavioux <pakmarios@gmail.com>
2019-09-26 21:40:10 +0300

    implmented a register form and laid the foundation for mongodb

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 let express = require('express');
-let mongoClient = require('mongodb').MongoClient;
-let url = 'mongodb://localhost/maviouxSiteClientsDB';
+let mongoose = require('mongoose');
 let app = express();
 
 const PORT = 5000;
+
+mongoose.connect('mongodb://localhost/testaroo');
+
+mongoose.connection.once('open', () => {
+    console.log("Connected to Mongo");
+}).on('error', () => {
+    console.log('Connection error: ' + error);
+});
 
 //Setup Pug for dynamic pages
 app.set('views', 'views');
@@ -16,9 +23,14 @@ app.get('/', (req, res) => {
     res.send('Hello World!')
 })
 
-// app.get('/register', (req, res) => {
-    
-// })
+app.get('/register', (req, res) => {
+    res.render('register');
+})
+
+//When pressing sign up redirect to home page
+app.post('/register', (req, res) => {
+    res.redirect('/');
+})
 
 // app.get('/pomodoro', (req, res) => {
 //     res.render('pomodoro', {title: 'Hello', message: x.toString()});
@@ -26,4 +38,6 @@ app.get('/', (req, res) => {
 
 app.listen(PORT, () => {
     console.log('Server started on localhost:' + PORT);
+}).on('error', () => {
+    console.log('Error deploying server - ');
 });

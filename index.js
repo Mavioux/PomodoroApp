bde1ffcfcd81b9304c9cc8a9f923eb2f53cd71c2
mavioux <pakmarios@gmail.com>
2019-09-26 20:35:34 +0300

    Served mavioux static page

--- index.js
+++ index.js
@@ -1,31 +1,29 @@
 let express = require('express');
+let mongoClient = require('mongodb').MongoClient;
+let url = 'mongodb://localhost/maviouxSiteClientsDB';
 let app = express();
 
 const PORT = 5000;
-
-let time = 60;
 
 //Setup Pug for dynamic pages
 app.set('views', 'views');
 app.set('view engine', 'pug');
 
-
+//Set the server to serve static pages from the public directory
 app.use(express.static('public'));
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
 })
 
+// app.get('/register', (req, res) => {
+    
+// })
 
-
-app.get('/pomodoro', (req, res) => {
-    res.render('pomodoro', {title: 'Hello', message: x.toString()});
-});
+// app.get('/pomodoro', (req, res) => {
+//     res.render('pomodoro', {title: 'Hello', message: x.toString()});
+// });
 
 app.listen(PORT, () => {
     console.log('Server started on localhost:' + PORT);
 });
-
-function decreaseTime () {
-    setTimeout(time = time - 1, 1000);
-}

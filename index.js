e5d2afce182bc5850c2e658e1f282574b64ef31c
mavioux <pakmarios@gmail.com>
2019-10-23 16:13:29 +0300

    registration

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
 let express = require('express');
 let mongoose = require('mongoose');
 const expressLayouts = require('express-ejs-layouts');
+const flash = require('connect-flash');
+const session = require('express-session');
+const passport = require('passport');
+var path = require('path'); 
 let app = express();
 const PORT = 5000;
+
+//Passport Config
+require('./config/passport')(passport);
 
 //Set DB
 const db = require('./config/keys').MongoURI;
@@ -15,6 +22,31 @@ mongoose.connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
 //EJS       
 app.use(expressLayouts);
 app.set('view engine', 'ejs');
+
+//Bodyparser
+app.use(express.urlencoded( { extended: false }));
+
+//Express Session Middleware
+app.use(session({
+    secret: 'keyboard cat',
+    resave: true,
+    saveUninitialized: true,
+  }))
+
+//Passport Middleware
+app.use(passport.initialize());
+app.use(passport.session());
+
+//Connect Flash Middleware
+app.use(flash());
+
+//Global Vars
+app.use((req, res, next) => {
+    res.locals.success_msg = req.flash('success_msg');
+    res.locals.error_msg = req.flash('error_msg');
+    res.locals.error = req.flash('error');
+    next();
+})
 
 //Set the server to serve static pages from the public directory
 app.use(express.static('public'));
--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const { ensureAuthenticated } = require('../config/auth');
 
 router.get('/', (req, res) => {
     res.render('landing-page');
@@ -8,5 +9,12 @@ router.get('/', (req, res) => {
 router.get('/pomodoro', (req, res) => {
     res.render('pomodoro', {title: 'Hello', message: x.toString()});
 });
+
+//Handle dashboard
+router.get('/dashboard', ensureAuthenticated, (req, res) => {
+    res.render('dashboard', {
+        name: req.user.name
+    });
+})
 
 module.exports = router;

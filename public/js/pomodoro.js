f3ea63d84dda11e0090d93d08a21ab22102ad753
mavioux <pakmarios@gmail.com>
2019-10-26 14:54:27 +0300

    finished countdown logic

--- public/js/pomodoro.js
+++ public/js/pomodoro.js
@@ -1,7 +1,15 @@
 let counter = 15 * 60
- let counterElement = document.getElementById("pomodoro-counter")
- let div = document.createElement("div");
- div.setAttribute('id', "added")
+diffInMins = 15
+let now = new Date()
+let endTime = new Date(now.getTime() + diffInMins*60000);
+console.log(now.getMinutes() + " : " + now.getSeconds())
+console.log(endTime.getMinutes() + " : " + endTime.getSeconds())
+
+let counterElement = document.getElementById("pomodoro-counter")
+let minutesDiv = document.createElement("div");
+let secondsDiv = document.createElement("div");
+minutesDiv.setAttribute('id', "minutes")
+secondsDiv.setAttribute('id', "seconds")
 
 
 setTimeout( () => {
@@ -11,12 +19,25 @@ setTimeout( () => {
 
 function countdown(c) {
     c = c - 1
-    console.log(c)
-    var cString = document.createTextNode(c.toString());
-    div.appendChild(cString)
-    counterElement.appendChild(div)
+    now = new Date()
+    timeDifferenceMinutes = Math.floor((endTime.getTime() - now.getTime()) / 1000 / 60)
+    timeDifferenceSeconds = (now.getSeconds() - endTime.getSeconds())
+    if(timeDifferenceSeconds > 0 ) {
+        timeDifferenceSeconds = 60 - timeDifferenceSeconds
+    }
+    else{
+        timeDifferenceSeconds = -timeDifferenceSeconds
+    }
+    console.log(timeDifferenceMinutes + " : " + timeDifferenceSeconds)
+    let timeInMinutesToString = document.createTextNode(timeDifferenceMinutes.toString());
+    let timeInSecondsToString = document.createTextNode(timeDifferenceSeconds.toString());
+    minutesDiv.appendChild(timeInMinutesToString)
+    secondsDiv.appendChild(timeInSecondsToString)
+    counterElement.appendChild(minutesDiv)
+    counterElement.appendChild(secondsDiv)
     setTimeout( () => {
-        div.removeChild(div.firstChild)
+        minutesDiv.removeChild(minutesDiv.firstChild)
+        secondsDiv.removeChild(secondsDiv.firstChild)
         countdown(c)
     }, 1000)
 }
